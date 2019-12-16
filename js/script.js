f4feb71e61cd1e8ca4f5b29dd80cd78e0a2dd122
Ahkubekov Alim <ahkubekov.alim@gmail.com>
2019-12-17 01:59:34 +0300

    styles for rekvisits

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+
 $(function(){
+    var clipboard =  new ClipboardJS('.clipboard');
+
+    function showCopiedMessage($element){
+        $element.append('<div class="copied">Скопировано</div>');
+        setTimeout(function(){
+            var $copied = $element.find('.copied');
+            $copied.remove();
+        }, 2000)
+    }
+
+    clipboard.on('success', function(e) {
+        console.info('Action:', e.action);
+        console.info('Text:', e.text);
+        console.info('Trigger:', e.trigger);
+        showCopiedMessage($(e.trigger));
+        e.clearSelection();
+    });
 
 });
